8301ae9ed0a9baf54cc56e9f399ccb11617f9c71
HappyLego214 <anthony21josh@gmail.com>
2023-05-11 19:04:18 +0800

    adds full validation API functionality in forms

--- scripts.js
+++ scripts.js
@@ -1,34 +1,70 @@
 const password = document.querySelector(".password");
 const pswCheck = document.querySelector(".pswCheck");
 const error = document.querySelector(".error");
-const error1 = document.querySelector(".error1");
+const checkError = document.querySelector(".checkError");
+const credential = document.querySelectorAll(".credential");
+
+credential.forEach((cred) => {
+    cred.addEventListener('input', () => {
+        let z = cred.nextElementSibling;
+        if (cred.validity.valid) {
+            z.textContent = "Valid";
+            z.classList.add("valid");
+            z.classList.remove("invalid");
+        } else {
+            showError(cred, z);
+        }
+    })
+});
 
 password.addEventListener('input', () => {
-    if (password.validity.valid) {
-        error.textContent = "Valid";
-        error.className = "error";
-        console.log("check1")
-    } else {
-        showError(password)
-        console.log("check2")
-    }
+    passwordInput();
+    passwordValid();
 });
 
 pswCheck.addEventListener('input', () => {
-    if (pswCheck.value == password.value) {
-        error1.textContent = "Valid";
-        error1.className = "error";
-        console.log("check3")
-    } else {
-        console.log("check4");
-    }
+    passwordValid();
 });
 
-function showError(e) {
-    if (e.validity.valueMissing) {
-        error.textContent = "Missing Required Value";
-    } else if (e.validity.patternMismatch) {
-        error.textContent = "Input Does Not Match";
-    } 
-    error.className = "errorActive";
+function passwordInput() {
+    if (password.validity.valid) {
+        error.textContent = "Valid";
+        error.classList.add("valid"); 
+        error.classList.remove("invalid");
+
+    } else {
+        showError(password, error)
+    }
+}
+
+function passwordValid() {
+    if (pswCheck.value == password.value && !(password.value == "")) {
+        checkError.textContent = "Valid";
+        checkError.classList.add("valid"); 
+        checkError.classList.remove("invalid");
+    } else if (pswCheck.value != password.value) {
+        checkError.textContent = "Password Does Not Match"
+        checkError.classList.add("invalid"); 
+        checkError.classList.remove("valid");
+    } else if (password.value == "") {
+        checkError.textContent = "Missing Password";
+        checkError.classList.add("invalid"); 
+        checkError.classList.remove("valid");
+    }
+}
+
+function showError(foo, bar) {
+    if (foo.validity.valueMissing) {
+        bar.textContent = "Missing Required Value";
+    } else if (foo.validity.patternMismatch) {
+        bar.textContent = "Please Enter With The Required Pattern";
+    } else if (foo.validity.tooShort) {
+        bar.textContent = "Item Too Short";
+    } else if (foo.validity.tooLong) {
+        bar.textContent = "Item Too Long";
+    } else {
+        bar.textContent = "Invalid Format";
+    }
+    bar.classList.add("invalid");
+    bar.classList.remove("valid");
 }
